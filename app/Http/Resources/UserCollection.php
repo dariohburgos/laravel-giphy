4b4b641d246bac148e677e855733e3bddeec6f58
Dario Burgos (Daro) <dariohburgos@gmail.com>
2024-04-03 09:53:45 -0300

    Initial commit
    Changes to be committed:
    	new file:   .editorconfig
    	new file:   .env.example
    	new file:   .gitattributes
    	new file:   .gitignore
    	new file:   README.md
    	new file:   app/Http/Controllers/Api/LoginController.php
    	new file:   app/Http/Controllers/Api/UserController.php
    	new file:   app/Http/Controllers/Controller.php
    	new file:   app/Http/Resources/UserCollection.php
    	new file:   app/Http/Resources/UserResource.php
    	new file:   app/Models/User.php
    	new file:   app/Providers/AppServiceProvider.php
    	new file:   artisan
    	new file:   bootstrap/app.php
    	new file:   bootstrap/cache/.gitignore
    	new file:   bootstrap/providers.php
    	new file:   composer.json
    	new file:   composer.lock
    	new file:   config/app.php
    	new file:   config/auth.php
    	new file:   config/cache.php
    	new file:   config/database.php
    	new file:   config/filesystems.php
    	new file:   config/logging.php
    	new file:   config/mail.php
    	new file:   config/queue.php
    	new file:   config/sanctum.php
    	new file:   config/services.php
    	new file:   config/session.php
    	new file:   database/.gitignore
    	new file:   database/factories/UserFactory.php
    	new file:   database/migrations/0001_01_01_000000_create_users_table.php
    	new file:   database/migrations/0001_01_01_000001_create_cache_table.php
    	new file:   database/migrations/0001_01_01_000002_create_jobs_table.php
    	new file:   database/migrations/2024_04_03_121342_create_personal_access_tokens_table.php
    	new file:   database/seeders/DatabaseSeeder.php
    	new file:   docker-compose.yml
    	new file:   package.json
    	new file:   phpunit.xml
    	new file:   public/.htaccess
    	new file:   public/favicon.ico
    	new file:   public/index.php
    	new file:   public/robots.txt
    	new file:   resources/css/app.css
    	new file:   resources/js/app.js
    	new file:   resources/js/bootstrap.js
    	new file:   resources/views/welcome.blade.php
    	new file:   routes/api.php
    	new file:   routes/console.php
    	new file:   routes/web.php
    	new file:   storage/app/.gitignore
    	new file:   storage/app/public/.gitignore
    	new file:   storage/framework/.gitignore
    	new file:   storage/framework/cache/.gitignore
    	new file:   storage/framework/cache/data/.gitignore
    	new file:   storage/framework/sessions/.gitignore
    	new file:   storage/framework/testing/.gitignore
    	new file:   storage/framework/views/.gitignore
    	new file:   storage/logs/.gitignore
    	new file:   tests/Feature/ExampleTest.php
    	new file:   tests/TestCase.php
    	new file:   tests/Unit/ExampleTest.php
    	new file:   vite.config.js

---
+++ app/Http/Resources/UserCollection.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Resources;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+class UserCollection extends ResourceCollection
+{
+  public $collects = UserResource::class;
+  public function toArray($request)
+  {
+    return parent::toArray($this->collects);
+  }
+}
